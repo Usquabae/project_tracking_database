3c1c9cb3587ba434f544c3c76e145deb1cfe39fd
Tom Pryor <tompryor@hotmail.co.uk>
2020-11-15 21:19:19 +0000

    additional home and about components

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
+// Below is importing the Angular modules for use
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
+// In the section below, add newly generated components to be 
+// dealt with by the routing module.
+
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+
+// To be routed, the path and component needs to be added to the 
+// Routes Array below.
 
 const routes: Routes = [
 
 {path: 'login', component: LoginComponent},
-{path: 'home' , component: HomeComponent}
+{path: 'home' , component: HomeComponent},
+{path: 'about' , component: AboutComponent}
 
 ];
 
